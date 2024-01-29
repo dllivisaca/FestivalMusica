c6dd5b8935c5dcc67b695188488260998170dbca
Daisy <daisynohelia@gmail.com>
2024-01-28 19:38:04 -0500

    actualizado hasta el video 197, agregando galeria HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
-const { src, dest, watch } = require("gulp"); //extrae la funcionalidad de gulp almacenado en node_modules
+const { src, dest, watch, parallel } = require("gulp"); //extrae la funcionalidad de gulp almacenado en node_modules
 //dentro de las llaves menciono las funciones de gulp que voy a usar
+
+//CSS 
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+
+//IMÁGENES
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     
@@ -13,11 +21,55 @@ function css(done) {
     done();
 }
 
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+
+    done();
+}
+
+function versionWebp ( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionAvif ( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function javascript( done ) {
+    src('src/js/**/*.js')
+        .pipe(dest('build/js'));
+
+        done();
+}
+
 function dev(done) {
-    watch('src/scss/**/*.scss', css)
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
 
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel ( imagenes, versionWebp, versionAvif, javascript, dev );
