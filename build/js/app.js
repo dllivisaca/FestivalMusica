79133a540dfffc28874990092c955a09c53a4b14
Daisy Llivisaca <daisynohelia@gmail.com>
2024-02-24 21:54:29 -0500

    actualizado hasta el video 204, agregando smooth scroll al header

--- build/js/app.js
+++ build/js/app.js
@@ -4,9 +4,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function iniciarApp() {
     crearGaleria();
+    scrollNav();
 }
 
-function crearGaleria () {
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth"});
+        })
+    });
+}
+
+function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
 
     for(let i=1;i<=12;i++) {
